9e8fe1c7ee6b8dc32dd30392929e89a40a487e06
Orlando Koh <pk15830@my.bristol.ac.uk>
2018-12-30 12:39:14 +0000

    test commit req invalid

--- cwproj/settings.py
+++ cwproj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%nxyg)j(6r^56$-97vdw_-qwix@r5%%dcaj9wz%rxcjdsho+#h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['eb-django-app-dev.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['wrost-env.pt2qdhgmiy.eu-west-2.elasticbeanstalk.com', 'localhost']
 
 
 # Application definition
