8e8dbfd8eb6b8bd429155e25707d6a97ad90003a
Orlando Koh <pk15830@my.bristol.ac.uk>
2019-01-02 17:37:04 +0000

    updated static location

--- cwproj/settings.py
+++ cwproj/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
 
 AWS_ACCESS_KEY_ID = config('ACCESS_KEY_ID')
