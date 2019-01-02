dd0fb1a9b1a593cd2257c63af1518dd8e5ddcad2
Orlando Koh <pk15830@my.bristol.ac.uk>
2019-01-02 18:36:16 +0000

    reverted to sqlite3

--- cwproj/settings.py
+++ cwproj/settings.py
@@ -90,14 +90,14 @@ WSGI_APPLICATION = 'cwproj.wsgi.application'
 #else:
 DATABASES = {
     'default': {
-#            'ENGINE': 'django.db.backends.sqlite3',
-#            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'wrost',
-        'USER': 'wrost',
-        'PASSWORD': 'testapp1111',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'wrost',
+#        'USER': 'wrost',
+#        'PASSWORD': 'testapp1111',
+#        'HOST': 'localhost',
+#        'PORT': '',
     }
 }
 
